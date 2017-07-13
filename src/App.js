f4dca32e0df139e3a83570a46ff37ddceab2e18b
Prerna <chaturvediprerna03@gmail.com>
2017-07-14 00:30:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from "axios";
-
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-
+import InputBox from './inputBox.js';
+import Header from './header.js';
 class App extends Component {
 		constructor(){
           super();
-          this.state = {products: [],flg:false};
+          this.state = {products: []};
+          this.getWeather = this.getWeather.bind(this);
         }
-		getTime(timeStamp=''){
+		getTime(timeStamp=''){ console.log(timeStamp);
 			var options = {
 				weekday: 'short',
 				month: 'short',
@@ -18,90 +18,55 @@ class App extends Component {
 			intlDate = new Intl.DateTimeFormat( undefined, options );
 			return intlDate.format( new Date( 1000 * timeStamp) );
 		}
-		handleKey(e)
+        getWeather(city)
 		{
-			if(e.which===13)
-				this.weatherForecast(e);
+            var data = [];
+            axios.get('http://openweathermap.org/data/2.5/forecast?q='+city+'&appid=b1b15e88fa797225412429c1c50c122a1')
+                .then(function (response) {
+
+                    var det = JSON.parse(response.request.response);
+                    for(let i = 0 ; i<det['list'].length; i++) {
+                        var options = {
+                                weekday: 'short',
+                                month: 'short',
+                                day: 'numeric',
+                            },
+                            intlDate = new Intl.DateTimeFormat( undefined, options );
+                        var dt =  intlDate.format( new Date( 1000 * det['list'][i]['dt']) );
+                        data[i] = {
+                            dt: dt,
+                            img: det['list'][i]['weather']['0']['icon'],
+                            temp: det['list'][i]['main']['temp_min'] + '/' + det['list'][i]['main']['temp_max'] + ' °C',
+                            weather: det['list'][i]['main']['temp_min'] + '°C',
+                            humid: det['list'][i]['main']['humidity'] + '%',
+                            wd_speed: det['list'][i]['wind']['speed'] + ' mps',
+                            fl: det['list'][i]['main']['temp'],
+                            chance: '-',
+                            amt: '-'
+                        };
+                    }
+                    this.setState({
+                        products:data
+                    });
+				}.bind(this))
+				.catch(function (error) {
+                });
 		}
 		weatherForecast(e){
-			var city = document.getElementById('city').value;
+			let city = document.getElementById('city').value;
 			if(city.trim()==="")
 				return false;
-			
-		   axios.get('http://openweathermap.org/data/2.5/forecast?q='+city+'&appid=b1b15e88fa797225412429c1c50c122a1')
-              .then(function (response) {
-				        console.log(response.request.response)
-						var array =[];	
-						var det = JSON.parse(response.request.response);
-						for(var i=0;i<5;i++){
-								var dt = this.getTime(det['list'][i]['dt']);
-								var a= ({dt:dt,
-										img:det['list'][i]['weather']['0']['icon'],
-										temp:det['list'][i]['main']['temp_min']+'/'+det['list'][i]['main']['temp_max']+ ' °C',
-										weather:det['list'][i]['main']['temp_min']+'°C',
-										humid:det['list'][i]['main']['humidity']+'%',
-										wd_speed:det['list'][i]['wind']['speed']+' mps',
-										fl:det['list'][i]['main']['temp'],
-										chance:'-',
-										amt:'-'
-										});
-										
-									array.push(a);	
-								}	
-								
-						this.setState({
-							products: array,
-							flg:true
-						});
-                /// and you can get response with this.state.todos in your className
-              }.bind(this))
-             .catch(function (error) {
-				console.log(error);
-             });
-		   
+
+            this.getWeather(city);
 		}
-	  imgCol(img){
-	  return '<img src="http://openweathermap.org/img/w/'+img+'.png" width="40" height="40"/>';
-	  }
-	  render() {
-		return (
+		render() {
+	  	return (
 		 <div className="container">
-			<div className="row">
-				<div className="col-md-6">
-					<h2>Search Weather here</h2>
-					<div id="custom-search-input">
-						<div className="input-group col-md-12">
-							<input id="city" type="text" className="form-control input-lg" placeholder="Eg: London" onKeyUp={this.handleKey.bind(this)} />
-							<span className="input-group-btn">
-	  <button className="btn btn-info btn-lg" type="button" onClick={this.weatherForecast.bind(this)}>
-									<i className="glyphicon glyphicon-search"></i>
-								</button>
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>			
-			{(this.state.products.length>0)?
-			<BootstrapTable data={ this.state.products }>
-				<TableHeaderColumn row='0' colSpan='1' dataField='dt' isKey={ true } ></TableHeaderColumn>
-				<TableHeaderColumn row='0' colSpan='3'>Conditions</TableHeaderColumn>
-				<TableHeaderColumn row='0' colSpan='3'>Comfort</TableHeaderColumn>
-				<TableHeaderColumn row='0' colSpan='2'>Precipitation</TableHeaderColumn>
-				<TableHeaderColumn row='1' dataField='dt'>Day</TableHeaderColumn>
-				<TableHeaderColumn row='1' dataField='img' dataFormat={this.imgCol.bind(this)}></TableHeaderColumn>
-				<TableHeaderColumn row='1' dataField='temp'>Temperature</TableHeaderColumn>
-				<TableHeaderColumn row='1' dataField='weather'>Weather</TableHeaderColumn>
-				<TableHeaderColumn row='1' dataField='fl'>Feels Like</TableHeaderColumn>
-				<TableHeaderColumn row='1' dataField='wd_speed'>Wind</TableHeaderColumn>
-				<TableHeaderColumn row='1' dataField='humid'>Humidity</TableHeaderColumn>
-				<TableHeaderColumn row='1' dataField='chance'>Chance</TableHeaderColumn>
-				<TableHeaderColumn row='1' dataField='amt'>Amount</TableHeaderColumn>
-			</BootstrapTable>
-			:""}
-			
+		 	 <InputBox weatherForecast={this.weatherForecast.bind(this)}/>
+			 <Header temperature_det={this.state.products}/>
+
 		</div>
 		);
 	  }
 }
-
 export default App;
