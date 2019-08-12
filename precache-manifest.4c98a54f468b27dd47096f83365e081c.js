4982424dd12a2eef750d6bbd8dd202a139d68536
prerna <chaturvediprerna03@gmail.com>
2019-08-12 15:52:13 +1200

    Updates

---
+++ precache-manifest.4c98a54f468b27dd47096f83365e081c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b817e605c354739109cab4cee9c2f69",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "2463f3d5afa5765d0242",
+    "url": "/weather_app/static/css/main.819901fc.chunk.css"
+  },
+  {
+    "revision": "475cda0fbc1bc02b07e7",
+    "url": "/weather_app/static/js/2.8b055146.chunk.js"
+  },
+  {
+    "revision": "2463f3d5afa5765d0242",
+    "url": "/weather_app/static/js/main.a7e502f4.chunk.js"
+  },
+  {
+    "revision": "5a93a33969a92b495477",
+    "url": "/weather_app/static/js/runtime~main.f31291a6.js"
+  }
+]);
