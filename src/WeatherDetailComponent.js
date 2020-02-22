1274d3eb73a09ffac6425f4ec091ce476e33300a
Prerna <chaturvediprerna03@gmail.com>
2020-02-22 15:23:02 +1300

    removed bootstrap package and use

--- src/WeatherDetailComponent.js
+++ src/WeatherDetailComponent.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { head } from 'lodash';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+    mainHeader:{
+        fontWeight:900,
+    },
+    header:{
+        fontWeight:800,
+    }
+});
 
 const WeatherDetailComponent = props =>{
+
+    const classes = useStyles();
+
     const getTime = timeStamp=>{
         var options = {
             weekday: 'short',
@@ -13,31 +35,44 @@ const WeatherDetailComponent = props =>{
         intlDate = new Intl.DateTimeFormat( undefined, options );
         return intlDate.format( new Date( 1000 * timeStamp) );
     }
-    const products = props.weatherDetail && 
-                        props.weatherDetail.map(obj=>{
-                            const weather = head(obj['weather']);
-                            return {
-                                'dt': getTime(obj.dt),
-                                'img': weather['icon'],
-                                'temperature': `${obj['main']['temp_min']}/${obj['main']['temp_max']} °C`,
-                                'weather':`${weather['main']}(${weather['description']})`,
-                                'humidity':obj['main']['humidity']+'%',
-                                'wind_speed':obj['wind']['speed']+' mps',
-                                'feels_like': obj['main']['temp'],
-                            }
-                        })
-    return <BootstrapTable data={ products }>
-                <TableHeaderColumn row='0' colSpan='1' dataField='dt' isKey={ true } ></TableHeaderColumn>
-                <TableHeaderColumn row='0' colSpan='3'>Conditions</TableHeaderColumn>
-                <TableHeaderColumn row='0' colSpan='3'>Comfort</TableHeaderColumn>
-                <TableHeaderColumn row='1' dataField='dt'>Day</TableHeaderColumn>
-                <TableHeaderColumn row='1' dataField='img' dataFormat={img=>`<img src="https://openweathermap.org/img/w/${img}.png" width="40" height="40"/>`}></TableHeaderColumn>
-                <TableHeaderColumn row='1' dataField='temperature'>Temperature</TableHeaderColumn>
-                <TableHeaderColumn row='1' dataField='weather'>Weather</TableHeaderColumn>
-                <TableHeaderColumn row='1' dataField='feels_like'>Feels Like</TableHeaderColumn>
-                <TableHeaderColumn row='1' dataField='wind_speed'>Wind</TableHeaderColumn>
-                <TableHeaderColumn row='1' dataField='humidity'>Humidity</TableHeaderColumn>
-        </BootstrapTable>
+    return  <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                <TableHead className={classes.header}>
+                    <TableRow>
+                        <TableCell colSpan="3" align="left"></TableCell>
+                        <TableCell className={classes.mainHeader} colSpan="2" align="left">Conditions</TableCell>
+                        <TableCell className={classes.mainHeader} colSpan="2" align="left">Comfort</TableCell>
+                    </TableRow>
+                </TableHead>    
+                <TableHead>
+                    <TableRow>
+                        <TableCell className={classes.header} align="left">Date & Time</TableCell>
+                        <TableCell className={classes.header} align="left"></TableCell>
+                        <TableCell className={classes.header} align="left">Temperature</TableCell>
+                        <TableCell className={classes.header} align="left">Weather</TableCell>
+                        <TableCell className={classes.header} align="left">Humidity</TableCell>
+                        <TableCell className={classes.header} align="left">Wind Speed</TableCell>
+                        <TableCell className={classes.header} align="left">Feels like</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {props.weatherDetail.map(obj=>{
+                    const weather=head(obj['weather']);
+                    return <TableRow key={obj.dt}>
+                                <TableCell align="left">{getTime(obj.dt)}</TableCell>
+                                <TableCell align="left"><img alt={obj.dt} src={`https://openweathermap.org/img/w/${weather['icon']}.png`} width="40" height="40"/></TableCell>
+                                <TableCell align="left">
+                                    {`${obj['main']['temp_min']}/${obj['main']['temp_max']} °C`}
+                                </TableCell>
+                                <TableCell align="left">{`${weather['main']} (${weather['description']})`}</TableCell>
+                                <TableCell align="left">{`${obj['main']['humidity']}%`}</TableCell>
+                                <TableCell align="left">{`${obj['wind']['speed']}mps`}</TableCell>
+                                <TableCell align="left">{obj['main']['temp']}</TableCell>
+                            </TableRow>
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>    
 }
 
 export default WeatherDetailComponent;
